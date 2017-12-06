2159299ec4319fa1c6d0b5caba813bdbf67627bb
gyn <781369549@qq.com>
2017-12-06 16:26:37 +0800

    修复bug

--- src/main/java/com/protobuf/test/GenerareClass.java
+++ src/main/java/com/protobuf/test/GenerareClass.java
@@ -9,11 +9,13 @@ import java.io.IOException;
 public class GenerareClass {
     public static void main(String[] args) {
         String protoFile = "person-entity.proto";
-        String path="C:/Users/Administrator/Documents/GitHub/protobuf-master";//项目地址
-        String strCmd =path +"/src/protoc.exe -I=./proto --java_out=./src/main/java ./proto/"+ protoFile;  //proto文件地址
+        
+        String path="C:\\Users\\Administrator\\Documents\\GitHub\\protobuf";//项目地址
+        String strCmd =path +"/google/src/protoc.exe -I=./proto --java_out=./src/main/java ./proto/"+ protoFile;  //proto文件地址
         try {
         	//通过执行cmd命令调用protoc.exe程序   
             Runtime.getRuntime().exec(strCmd);
+            System.out.println(protoFile+".java 生成成功！");
         } catch (IOException e) {
             e.printStackTrace();
         }
